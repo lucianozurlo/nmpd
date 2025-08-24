d57d5ea77488aff428eef18b9a403ec5354af10f
Luciano Zurlo <luciano@aiMac.local>
2025-08-23 21:16:01 -0300

    update

--- 4k-3/js/video-delay.js
+++
@@ -1,6 +0,0 @@
-var videoDelay = document.getElementById ('loop-massage');
-videoDelay.addEventListener ('canplay', function () {
-  setTimeout (function () {
-    videoDelay.play ();
-  }, 111000);
-});
